65ed9e686a07056e6d3150dd737bb7ab8d07de73
mwadia <mohammadalwadia@gmail.com>
2023-02-19 20:45:56 +0200

    all

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/my-app',
-  target: 'experimental-serverless-trace',
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      // Add any other pages you want to export
-    };
-  },
 };
 
 module.exports = nextConfig;
